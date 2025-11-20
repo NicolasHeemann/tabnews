8080aa306e5d8ccd8b2366bacc6268b7e4ceb739
NicolasHeemann <129404569+NicolasHeemann@users.noreply.github.com>
2025-11-20 23:42:43 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Teste 2</h1>
+function Home() {
+  return <h1>Teste 2</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("identação errada");
+}
 export default Home;
